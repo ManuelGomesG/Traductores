fd4593559f426ae767f51d2e2dacaf6886cfaeeb
Manuel <manuelgomesg@gmail.com>
2016-05-24 16:25:24 -0400

    estructuras basicas sin operaciones

---
+++ structs.py
@@ -0,0 +1,58 @@
+
+
+
+class BinaryOp (Expr):
+	def __init__(self,left,right,op,opV,typ):
+		self.left=left
+		self.right=right
+		self.op=op
+		self.opV=opV
+		self.type=typ
+
+
+	def getValues(self):
+		pass
+
+class UnaryOp(Expr):
+	def __init__(self,op,opV,term,typ):
+		self.op=op
+		self.term=typ
+		self.typ=typ
+
+	def getValue():
+		pass
+
+class Number(Expr):
+	def __init__(self,value):
+		self.value=value
+		self.type="Número"
+
+class Bool(Expr):
+	def __init__(self,value):
+		self.value=value
+		self.type="Booleano"
+
+class Ident(Expr):
+	def __init__(self,value):
+		self.value=value
+		self.type="Identificador"
+
+class InstrTree():
+	def __init__(self,token=None,child=None,instr=None):
+		self.token=token
+		if child:
+			self.child=child
+		else:
+			self.child=[]
+		self.instr=instr
+
+	def addChildren(self,children):
+		self.child=child
+
+	def addToken(self,token):
+		self.token=token
+
+	def addInstr(self,instr):
+		self.instr=instr
+
+
